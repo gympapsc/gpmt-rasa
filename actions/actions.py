ec3726fe08a20a80b746ec4496196a72e1c6e14a
hakrac <hakimrachidi02@gmail.com>
2021-05-23 15:13:49 +0200

    Try default state

--- actions/actions.py
+++ actions/actions.py
@@ -1,121 +1,27 @@
-from datetime import datetime
+# This files contains your custom actions which can be used to run
+# custom Python code.
+#
+# See this guide on how to implement these action:
+# https://rasa.com/docs/rasa/custom-actions
 
-from typing import Any, Text, Dict, List
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.events import FollowupAction, SlotSet, AllSlotsReset
-from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.forms import FormValidationAction, REQUESTED_SLOT
+# This is a simple example for a custom action which utters "Hello World!"
 
-from actions.store import Store
-
-import logging
-
-logger = logging.getLogger(__name__)
-
-logger.info("TEST")
-
-store = Store()
-
-class ActionAddMicturition(Action):
-    
-    def name(self):
-        return "action_add_micturition"
-    
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any]
-    ):
-        date = tracker.get_slot("time")
-        print(date)
-        store.create_micturition(tracker.sender_id, date)
-
-        dispatcher.utter_message(response="utter_confirm")
-        return [SlotSet(key="time")]
-
-class ActionAddDrinking(Action):
-
-    def name(self):
-        return "action_add_drinking"
-    
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any]
-    ):
-        date = tracker.get_slot("time")
-        amount = tracker.get_slot("amount")
-        store.create_drinking(tracker.sender_id, date, amount)
-
-        dispatcher.utter_message(response="utter_confirm")
-        return [
-            SlotSet(key="time"), 
-            SlotSet(key="amount")
-        ]
-
-class ActionExitQuestionnaire(Action):
-
-    def name(self):
-        return "action_exit_questionnaire"
-
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any]
-    ):
-        return [ SlotSet("questionnaire", False) ]
-
-class ValidateQuestionnaireForm(FormValidationAction):
-
-    def name(self):
-        return "validate_form_questionnaire"
-    
-    async def run(
-        self, 
-        dispatcher: CollectingDispatcher, 
-        tracker: Tracker, 
-        domain: Dict[Text, Any]
-    ):
-        validation_events = await self.validate(dispatcher, tracker, domain)
-
-        question_id = tracker.get_slot("question_id")
-        logger.info("QUESTION ID %s", question_id)
-        if question_id is None:
-            question = store.get_root_question()
-            logger.info("QUESTION %s", question["name"])
-        else:
-            last_question = store.get_question(question_id)
-            logger.info("LAST QUESTION %s", last_question["name"])
-            answer = tracker.get_slot(str(last_question["name"]))
-            if answer is None:
-                question = last_question
-            else:
-                logger.info("LAST ANSWER %s", answer)
-                store.save_answer(tracker.sender_id, question_id, answer)
-                question = store.get_next_question(question_id, answer)
-                if question is None:
-                    return [
-                        SlotSet(REQUESTED_SLOT, None)
-                    ]
-                logger.info("NEXT QUESTION %s", question["name"])
-
-        logger.info("ADD SLOT %s", str(question["_id"]))
-        
-        return validation_events + [
-            SlotSet("question_id", str(question["_id"])),
-            SlotSet(REQUESTED_SLOT, question["name"])
-        ]
-    
-    def validate_disease(
-        self,
-        slot_value: Any,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any]
-    ):
-        logger.info("SLOT VALUE %s", slot_value)
-        return {"disease": slot_value}
+# from typing import Any, Text, Dict, List
+#
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+#
+#
+# class ActionHelloWorld(Action):
+#
+#     def name(self) -> Text:
+#         return "action_hello_world"
+#
+#     def run(self, dispatcher: CollectingDispatcher,
+#             tracker: Tracker,
+#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+#
+#         dispatcher.utter_message(text="Hello World!")
+#
+#         return []
